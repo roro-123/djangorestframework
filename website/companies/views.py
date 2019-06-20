112994d3133c975489cf503b334b518d26854276
Warui Ngugi <waruingugientp@gmail.com>
2019-06-20 12:55:49 +0300

    First Commit

---
+++ website/companies/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Stock
+from .serializers import StockSerializer 
+
+
+#List all stocks or create a new one
+#Stocks/
+class StockList(APIView):
+    def get(self, request):
+        stocks = Stock.objects.all()
+        serializer = StockSerializer(stocks, many = True)
+        return Response(serializer.data)
+
+
+    def post(self):
+        pass
+
+
+
+
